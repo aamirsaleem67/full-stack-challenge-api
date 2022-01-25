23037c6de78c74efff36b66dcccd9a21fc89e321
Aamir Saleem <aamirsaleem167@gmail.com>
2022-01-25 13:44:08 +0500

    [FIX] - Allow cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.use(helmet()); // security
   app.setGlobalPrefix('/api/v1');
@@ -23,6 +23,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/api-docs', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
